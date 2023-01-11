e03ea4654589802e6a3ab25981e2ec9283b26ec1
nichakornchaisuwan <76930990+nichakornchaisuwan@users.noreply.github.com>
2023-01-12 00:44:41 +0700

    Create __init__.py

---
+++ models/backbone/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE for details)
+# -----------------------------------------------------------------------------
+
+"""PyTorch models."""
+
+from .inception_v3 import InceptionV3
